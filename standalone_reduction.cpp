385546b118a8aa938a64b1ae061158822f973654
GG-LLL <74540180+GG-LLL@users.noreply.github.com>
2024-10-09 11:58:16 +0100

    Update standalone_reduction.cpp

--- standalone_reduction.cpp
+++ standalone_reduction.cpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Program:     standalone_reduction
 
- * Copyright (C) 2023; Sanjay Bhattacherjee, Jack Moyler.
+ * Copyright (C) 2023;
 
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
